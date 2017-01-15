07ca2f5cb33bc62d877a3259c4e01d9ff9f3894b
Imarotur <ririzrizqiyah23@gmail.com>
2017-01-15 09:29:49 +0700

    Add files via upload
    ini contact

---
+++ contact.php
@@ -0,0 +1,73 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<!--
+Project      : Data Karyawan CRUD MySQLi (Create, read, Update, Delete) PHP, MySQLi dan Bootstrap
+Author		 : Hakko Bio Richard, A.Md
+Website		 : http://www.niqoweb.com
+Blog         : http://www.acchoblues.blogspot.com
+Email	 	 : hakkobiorichard[at]gmail.com
+-->
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+
+	<!-- Bootstrap -->
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	
+	<style>
+		.content {
+			margin-top: 80px;
+		}
+		body {
+			background-color:#eee;
+		}
+	</style>
+	
+	<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+</head>
+<body>
+	
+	<div class="row">
+		
+			<h2>&nbsp;</h2>
+			<div class="inner cover">
+            <h1 class="cover-heading text-center">Contact Us</h1>
+            <p>&nbsp;</p>
+              
+               <div class="row">
+        <div class="col-lg-4">
+          <img class="img-circle" src="images/f.png" alt="Generic placeholder image" width="140" height="140">
+          <h2>Facebook</h2>
+          <p>Temukan kami di Facebook, mari mengenal lebih dekat.</p>
+            <p><button type="button" class="btn btn-lg btn-primary"><a href="https://www.facebook.com/naufalhaedirhilal">Facebook</a></button></p>
+        </div><!-- /.col-lg-4 -->
+        <div class="col-lg-4">
+          <img class="img-circle" src="images/i.png" alt="Generic placeholder image" width="140" height="140">
+          <h2>Instagram</h2>
+          <p>Temukan kami di Instagram, Follow me.</p>
+          <p><button type="button" class="btn btn-lg btn-default">Instagram</button></p>
+        </div><!-- /.col-lg-4 -->
+        <div class="col-lg-4">
+          <img class="img-circle" src="images/t.png" alt="Generic placeholder image" width="140" height="140">
+          <h2>Twitter</h2>
+          <p>Temukan kami di Twitter, Just Follow me.</p>
+          <p><button type="button" class="btn btn-lg btn-info">Twitter</button></p>
+        </div><!-- /.col-lg-4 -->
+      </div><!-- /.row -->
+              
+      </div>
+           
+			<br />
+		
+</div>
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
